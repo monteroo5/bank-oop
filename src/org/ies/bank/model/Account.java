2f4db1a48d04f8e099b8eae98828488dd382a44c
MikelSV <mikel.san@educa.madrid.org>
2024-12-02 09:27:03 +0100

    POJOs y showAccounts

---
+++ src/org/ies/bank/model/Account.java
@@ -0,0 +1,67 @@
+package org.ies.bank.model;
+
+import java.util.Objects;
+
+public class Account {
+    private String iban;
+    private double balance;
+    private Customer customer;
+
+    public Account(String iban, double balance, Customer customer) {
+        this.iban = iban;
+        this.balance = balance;
+        this.customer = customer;
+    }
+
+    public void showInfo() {
+        System.out.println("IBAN: " + iban +
+                ". Saldo: " + balance +
+                ". NIF cliente: " + customer.getNif()
+        );
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Double.compare(balance, account.balance) == 0 && Objects.equals(iban, account.iban) && Objects.equals(customer, account.customer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(iban, balance, customer);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "iban='" + iban + '\'' +
+                ", balance=" + balance +
+                ", customer=" + customer +
+                '}';
+    }
+}
---
+++ src/org/ies/bank/model/Bank.java
@@ -0,0 +1,56 @@
+package org.ies.bank.model;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Bank {
+    private String name;
+    private Account[] accounts;
+
+    public Bank(String name, Account[] accounts) {
+        this.name = name;
+        this.accounts = accounts;
+    }
+
+    public void showAccounts() {
+        for (var account: accounts) {
+            account.showInfo();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account[] getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Account[] accounts) {
+        this.accounts = accounts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Bank bank = (Bank) o;
+        return Objects.equals(name, bank.name) && Objects.deepEquals(accounts, bank.accounts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, Arrays.hashCode(accounts));
+    }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "name='" + name + '\'' +
+                ", accounts=" + Arrays.toString(accounts) +
+                '}';
+    }
+}
